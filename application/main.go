13373d10477bcd3eda6c2873edda4343f78947ce
muthur-dev <muthurikennedy082@gmail.com>
2025-10-05 05:17:32 +0300

    test: Test workflow with new vault creds

--- application/main.go
+++ application/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-	// Load environment variables from .env file if it exists
+	// Load environment variables from .env file ifor db connection
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found, using environment variables")
 	}
